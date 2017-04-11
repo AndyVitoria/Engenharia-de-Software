cc1f5140a90b8d8e6bde66c6898257ef9b2898c9
andrebvitoria <andre_davitoria@hotmail.com>
2017-04-11 09:33:07 -0300

    Delete Acentos.py

--- src/Acentos.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from unicodedata import normalize
-
-def remover_acentos(txt):
-    return normalize('NFKD', txt).encode('ASCII','ignore').decode('ASCII')
-
-
